22810777841c8c3cae308f653da048206d840d79
6mahdapihka9 <shnyger@gmail.com>
2022-10-09 12:14:26 +0300

    implemented jwt strategies in auth controller, created tokenGenerator

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,32 +1,89 @@
 import { NextFunction, Request, Response } from 'express'
+import passport from '../libs/passport';
+import jwt from 'jsonwebtoken';
+import { IUser } from '../models/UserModel';
+import tokenGenerator from '../utils/tokenGenerator';
 
 const AuthController = {
   async signUp (req: Request, res: Response, next: NextFunction) {
     try {
-
-      return res.status(200).json({ message: 'signUp' })
+      await passport.authenticate(
+        'signUp',
+        { session: false },
+        async (err, user, info) => {
+          if (err) throw err
+          if (!user) {
+            return res.status(400).json({ info })
+          }
+          res.json({
+            message: 'successful registration'
+            // token: _tokenGeneration(user)
+          })
+        }
+      )(req, res, next)
     } catch (err: any) {
-      return res.status(500).json({ message: 'error' })
+      return res.status(500).json({ error: 'server error', err })
     }
   },
 
   async signIn (req: Request, res: Response, next: NextFunction) {
     try {
-
-      return res.status(200).json({ message: 'signIn' })
+      await passport.authenticate('signIn', async (err, user, info) => {
+        if (err) throw err
+        if (!user) {
+          return res.status(400).json({ info })
+        }
+        req.login(user, { session: false }, async error => {
+          if (error) return next(error)
+          return res.json({
+            message: 'successful authorization',
+            token: _tokenGeneration(user)
+          })
+        })
+      })(req, res, next)
     } catch (err: any) {
-      return res.status(500).json({ message: 'error' })
+      return res.status(500).json({ error: 'server error', err })
     }
   },
 
   async checkJwt (req: Request, res: Response) {
-    try {
+    const token = req.headers.authorization?.split(' ')?.[1]
 
-      return res.status(200).json({ message: 'check Jwt' })
-    } catch (err: any) {
-      return res.status(500).json({ message: 'error' })
+    if (!token) {
+      return res.status(400).json({
+        error: 'token not provided'
+      })
     }
+    const decodedToken = jwt.decode(token)
+
+    jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`, (err, decoded) => {
+      if (err != null) {
+        if (err instanceof jwt.TokenExpiredError) {
+          return res.status(400).json({
+            error: 'token expired',
+            err
+          })
+        } else if (err instanceof jwt.JsonWebTokenError) {
+          return res.status(400).json({
+            error: 'token malformed'
+          })
+        } else {
+          return res.status(400).json({
+            error: 'token invalid'
+          })
+        }
+      }
+
+      return res.status(200).json({
+        message: 'token is valid'
+      })
+    })
   }
+}
+
+function _tokenGeneration (user: IUser) {
+  const body = { _id: user._id, email: user.email }
+  return tokenGenerator.accessToken(body)
 }
 
 export default AuthController
