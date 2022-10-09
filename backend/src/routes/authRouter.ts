4bda25760c8d1dc822bfb91acb1ab1395ee8cf4c
6mahdapihka9 <shnyger@gmail.com>
2022-10-09 10:44:48 +0300

    connected routes with endpoints

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import AuthController from "../controllers/AuthController";
 
 const authRouter = express.Router()
 
-authRouter.get('/check-token', () => {})
-authRouter.post('/sign-up', () => {})
-authRouter.post('/sign-in', () => {})
+authRouter.get('/check-token', AuthController.checkJwt)
+authRouter.post('/sign-up', AuthController.signUp)
+authRouter.post('/sign-in', AuthController.signIn)
 
 export default authRouter
--- backend/src/routes/passwordsRouter.ts
+++ backend/src/routes/passwordsRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import PasswordController from "../controllers/PasswordController";
 
 const passwordsRouter = express.Router();
 
-passwordsRouter.put('/create', () => {});
-passwordsRouter.get('/read', () => {});
-passwordsRouter.patch('/update', () => {});
-passwordsRouter.delete('/delete', () => {});
+passwordsRouter.put('/create', PasswordController.createPassword);
+passwordsRouter.get('/read', PasswordController.readPasswords);
+passwordsRouter.patch('/update', PasswordController.updatePassword);
+passwordsRouter.delete('/delete', PasswordController.deletePassword);
 
 export default passwordsRouter;
