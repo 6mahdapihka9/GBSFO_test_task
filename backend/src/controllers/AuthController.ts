a2a728d7d74225a64fe9738c050f1c4008afec6b
6mahdapihka9 <shnyger@gmail.com>
2022-10-09 10:52:29 +0300

    all endpoints are tested in postman

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -3,20 +3,29 @@ import { NextFunction, Request, Response } from 'express'
 const AuthController = {
   async signUp (req: Request, res: Response, next: NextFunction) {
     try {
+
+      return res.status(200).json({ message: 'signUp' })
     } catch (err: any) {
+      return res.status(500).json({ message: 'error' })
     }
   },
 
   async signIn (req: Request, res: Response, next: NextFunction) {
     try {
 
+      return res.status(200).json({ message: 'signIn' })
     } catch (err: any) {
-
+      return res.status(500).json({ message: 'error' })
     }
   },
 
   async checkJwt (req: Request, res: Response) {
+    try {
 
+      return res.status(200).json({ message: 'check Jwt' })
+    } catch (err: any) {
+      return res.status(500).json({ message: 'error' })
+    }
   }
 }
 
--- backend/src/routes/passwordsRouter.ts
+++ backend/src/routes/passwordsRouter.ts
@@ -5,7 +5,7 @@ const passwordsRouter = express.Router();
 
 passwordsRouter.put('/create', PasswordController.createPassword);
 passwordsRouter.get('/read', PasswordController.readPasswords);
-passwordsRouter.patch('/update', PasswordController.updatePassword);
-passwordsRouter.delete('/delete', PasswordController.deletePassword);
+passwordsRouter.patch('/update/:id', PasswordController.updatePassword);
+passwordsRouter.delete('/delete/:id', PasswordController.deletePassword);
 
 export default passwordsRouter;
