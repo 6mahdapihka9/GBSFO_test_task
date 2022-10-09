867f3c6b26cbce56f790606074672270f35f7da0
6mahdapihka9 <shnyger@gmail.com>
2022-10-09 12:45:07 +0300

    implemented all REST end points via CRUD methods to operate with mongoDB

--- backend/src/controllers/PasswordController.ts
+++ backend/src/controllers/PasswordController.ts
@@ -1,36 +1,70 @@
 import { Response, Request } from 'express';
+import PasswordModel from "../models/PasswordModel";
 
 const PasswordController = {
   async createPassword (req: Request, res: Response) {
     try {
+      const _id = req.user;
+      const password = JSON.parse(req.body.password as string);
+      if (!password || !password.name || !password.value) {
+        return res.status(403).json({ message: 'error: bad request' });
+      }
 
-      return res.status(200).json({ message: 'create' })
+      await (new PasswordModel({
+        name: password.name,
+        value: password.value,
+        author: _id
+      })).save();
+
+      return res.status(200).json({ message: 'created' });
     } catch (err: any) {
-      return res.status(500).json({ message: 'error' })
+      return res.status(500).json({ error: 'other server error', err });
     }
   },
   async readPasswords (req: Request, res: Response) {
     try {
+      const _id = req.user;
 
-      return res.status(200).json({ message: 'read' })
+      const list = await PasswordModel.find({ author: _id });
+
+      return res.status(200).json({ message: 'read', list });
     } catch (err: any) {
-      return res.status(500).json({ error: 'other server error', err })
+      return res.status(500).json({ error: 'other server error', err });
     }
   },
   async updatePassword (req: Request, res: Response) {
     try {
+      const password = JSON.parse(req.body.password as string);
+      const passwordID = req.params.id;
 
-      return res.status(200).json({ message: 'update' })
+      if (!password || !password.name || !password.value) {
+        return res.status(403).json({ message: 'error: bad request' });
+      }
+
+      const isSuccess = await PasswordModel.findByIdAndUpdate(passwordID, {
+        ...password
+      });
+      if (!isSuccess) {
+        return res.status(200).json({ message: 'error: password was not found' });
+      }
+
+      return res.status(200).json({ message: 'updated' });
     } catch (err: any) {
-      return res.status(500).json({ error: 'other server error', err })
+      return res.status(500).json({ error: 'other server error', err });
     }
   },
   async deletePassword (req: Request, res: Response) {
     try {
+      const passwordID = req.params.id;
 
-      return res.status(200).json({ message: 'delete' })
+      const isSuccess = await PasswordModel.findByIdAndRemove(passwordID);
+      if (!isSuccess) {
+        return res.status(200).json({ message: 'error: password was not found' });
+      }
+
+      return res.status(200).json({ message: 'deleted' });
     } catch (err: any) {
-      return res.status(500).json({ error: 'other server error', err })
+      return res.status(500).json({ error: 'other server error', err });
     }
   }
 }
